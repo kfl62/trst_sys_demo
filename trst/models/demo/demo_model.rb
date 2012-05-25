87d069e9ea8634d045f77c25739e4220cb8aeb28
kfl62 <kfl62g@gmail.com>
2012-05-25 14:59:34 +0300

    models: demo models (naming convention)

---
+++ trst/models/demo/demo_model.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+module Trst
+  # Camelcase model name first tag is Trst.firm.model_path
+  # CRUD path trst/views/system/trst/demo/model
+  class DemoModel
+  end
+  # Camelcase model name first tag is Trst.firm.model_path
+  # CRUD path trst/views/system/trst/demo/model_embedded
+  class DemoModelEmbedded
+  end
+end
